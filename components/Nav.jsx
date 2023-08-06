6b4486e5f60611bb58604e906e85ba6c201bdd02
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-06 14:10:22 +0530

    nav-for-mobile-added@04:00:

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,51 +4,67 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Nav = () => {
-    const userLoggedIn = true
+  const userLoggedIn = true;
 
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href='/' className='flex gap-2 flex-center'>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src='/assets/images/logo.svg'
-          alt='logo'
+          src="/assets/images/logo.svg"
+          alt="logo"
           width={30}
           height={30}
-          className='object-contain'
+          className="object-contain"
         />
-        <p className='logo_text'>Promptopia</p>
+        <p className="logo_text">Promptopia</p>
       </Link>
 
       {/* Desktop Navigation */}
-      <div className='sm:flex hidden'>
+      <div className="sm:flex hidden">
         {userLoggedIn ? (
-          <div className='flex gap-3 md:gap-5'>
-            <Link href='/create-prompt' className='black_btn'>
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
 
-            <button type='button' onClick={() => {}} className='outline_btn'>
+            <button type="button" onClick={() => {}} className="outline_btn">
               Sign Out
             </button>
 
-            <Link href='/profile'>
+            <Link href="/profile">
               <Image
-               src='/assets/images/logo.svg'
+                src="/assets/images/logo.svg"
                 width={37}
                 height={37}
-                className='rounded-full'
-                alt='profile'
+                className="rounded-full"
+                alt="profile"
               />
             </Link>
           </div>
         ) : (
-          <>
-          {/* List down all provider for logIn and signUp */}
-          </>
+          <>{/* List down all provider for logIn and signUp */}</>
         )}
       </div>
 
-
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {userLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown(!toggleDropdown)}
+            />
+          </div>
+        ) : (
+          <>
+            {/* List down provider for signIn  */}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
