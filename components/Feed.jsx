228b6e89069311079dabfde1f8dcaa8a342297eb
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-08 13:09:55 +0530

    feed-component-added@01:30

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,29 @@
+"use client";
+
+import { useState } from "react";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  // Search states
+  const [searchText, setSearchText] = useState("");
 
-export default Feed
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      {/* All Prompts */}
+    </section>
+  );
+};
+
+export default Feed;
